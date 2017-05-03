896588618406714026c0d8d41b88d90a4cc96d60
vagabond <norbixin@live.com>
2017-05-03 16:29:09 +0300

    hosting

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -3,7 +3,7 @@ import org.sql2o.*;
 public class DB {
   // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/barber_shop", "vagabond","472113");
   
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql:ec2-54-227-237-223.compute-1.amazonaws.com:5432/d5nagp19ddk2no","xxnncgyhmfzfps","30cb40aae6f46443d8b0efcf840d70eab0b38534951965eecfe54115dcbddb22");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-227-237-223.compute-1.amazonaws.com:5432/d5nagp19ddk2no","xxnncgyhmfzfps","30cb40aae6f46443d8b0efcf840d70eab0b38534951965eecfe54115dcbddb22");
 }
 
 // postgres://xxnncgyhmfzfps:30cb40aae6f46443d8b0efcf840d70eab0b38534951965eecfe54115dcbddb22@ec2-54-227-237-223.compute-1.amazonaws.com:5432/d5nagp19ddk2no
